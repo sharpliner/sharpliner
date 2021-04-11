19954be5ff85ea7638f1969dfacb880f6235bbdc
Premek Vysoky <premek.vysoky@microsoft.com>
2021-04-11 17:28:03 +0200

    Initialize solution

---
+++ Sharpliner.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sharpliner.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
