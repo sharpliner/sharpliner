04a49ad527795a657e6bdecd23744698a6fb3863
Premek Vysoky <premek.vysoky@microsoft.com>
2021-04-15 22:11:01 +0200

    Move optional parameters out of ctor

--- src/Sharpliner.Model/Job.cs
+++ src/Sharpliner.Model/Job.cs
@@ -3,20 +3,40 @@ using System.Collections.Generic;
 
 namespace Sharpliner.Model
 {
-    public record Job(
-        string Name,
-        string DisplayName,
-        IEnumerable<string> DependsOn,
-        string Condition,
-        JobStrategy Strategy,
-        Pool Pool,
-        ContainerReference Container,
-        TimeSpan Timeout,
-        TimeSpan CancelTimeout,
-        IEnumerable<Variable> Variables,
-        IEnumerable<Step> Steps,
-        JobWorkspace Workspace = JobWorkspace.Outputs,
-        bool ContinueOnError = false);
 
     // TODO: missing properties Uses, Services
+    public record Job
+    {
+        public string Name { get; }
+
+        public string DisplayName { get; }
+
+        public IEnumerable<string>? DependsOn { get; init; }
+
+        public string? Condition { get; init; }
+
+        public JobStrategy? Strategy { get; init; }
+
+        public Pool? Pool { get; init; }
+
+        public ContainerReference? Container { get; init; }
+
+        public TimeSpan? Timeout { get; init; }
+
+        public TimeSpan? CancelTimeout { get; init; }
+
+        public IEnumerable<Variable>? Variables { get; init; }
+
+        public IEnumerable<Step>? Steps { get; init; }
+
+        public JobWorkspace Workspace { get; init; } = JobWorkspace.Outputs;
+
+        public bool ContinueOnError { get; init; } = false;
+
+        public Job(string name, string displayName)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            DisplayName = displayName ?? throw new ArgumentNullException(nameof(displayName));
+        }
+    }
 }
