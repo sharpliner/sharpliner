3454f9d6cc7199fc36523b459b4c49fe43f4e411
Premek Vysoky <premek.vysoky@microsoft.com>
2021-04-26 21:37:25 +0200

    Traverse to root definition

--- src/Sharpliner.Model/AzureDevOps/AzureDevOpsPipeline.cs
+++ src/Sharpliner.Model/AzureDevOps/AzureDevOpsPipeline.cs
@@ -1,14 +1,35 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Sharpliner.Model.AzureDevOps
 {
     public record AzureDevOpsPipeline
     {
+        private List<ConditionedDefinition<VariableBase>> _variables = new();
+
         public string? Name { get; init; }
 
         public Resources? Resources { get; init; }
 
-        public List<ConditionedDefinition<VariableBase>> Variables { get; init; } = new();
+        public List<ConditionedDefinition<VariableBase>> Variables
+        {
+            get => _variables;
+            init => _variables = value
+                .Select(v =>
+                {
+                    // Find the root definitions
+                    // This is because when we define a tree of conditional definitions,
+                    // the expression returns the leaf definition so we have to move back
+                    // up to the top-level definitions.
+                    while (v.Parent != null)
+                    {
+                        v = (ConditionedDefinition<VariableBase>)v.Parent;
+                    }
+
+                    return v;
+                })
+                .ToList();
+        }
 
         public Trigger? Trigger { get; init; } = null;
 
