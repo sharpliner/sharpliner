af0aa7836345474d7751688d5354fff2387562b3
Manuel de la Pena <mandel@microsoft.com>
2021-05-04 23:14:34 -0400

    [Mode] GithubAction - Add permissions to the workflow.
    Allow to set the persmissions for a workflow, there are several ways to
    use the API.
    
    1. Provide each scope we want to Read or Write
    
    ```csharp
    Permissions =
    {
        Write =
        {
            GitHubPermissionScope.Actions,
        },
        Read = {
            GitHubPermissionScope.Actions,
            GitHubPermissionScope.Contents
        }
    }
    ```
    
    2. Read All
    
    Use the fluent API
    
    ```csharp
    Permissions = new Permissions().All(GitHubPermission.Read)
    ```
    
    3. Write All
    
    Use the fluent API
    
    ```csharp
    Permissions = new Permissions().All(GitHubPermission.Write)
    ```
    
    4. Read and Write All
    
    Use the fluent API
    
    ```csharp
    Permissions = new Permissions().All(GitHubPermission.Write).All(GitHubPermission.Read)
    ```
    
    5. A mix of Read/Write All and more specific
    
    Use a mix of declarative + fluet:
    ```csharp
    Permissions = new Permissions
    {
      Read =
      {
        GitHubPermissionScope.Actions,
      }
    }.All(GitHubPermission.Write)
    ```
    
    The above API will also be used for jobs.

---
+++ src/Sharpliner.Model/GHActions/Permissions.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+
+namespace Sharpliner.Model.GHActions
+{
+    public enum GitHubPermissionScope
+    {
+        Actions,
+        Checks,
+        Contents,
+        Deployments,
+        Issues,
+        Packages,
+        PullRequests,
+        RepositoryProjects,
+        SecurityEvents,
+        Statuses,
+    }
+
+    public enum GitHubPermission
+    {
+        None,
+        Read,
+        Write
+    }
+
+    public record Permissions
+    {
+        public HashSet<GitHubPermissionScope> Read { get; } = new();
+        public HashSet<GitHubPermissionScope> Write { get; } = new();
+
+        public Permissions All(GitHubPermission permission)
+        {
+            // loop and add, we are not worried because it is a hasset, we add
+            // to the correct hashset or remove from both in the case of None
+            foreach (var scope in Enum.GetValues<GitHubPermissionScope>())
+            {
+                switch (permission)
+                {
+                    case GitHubPermission.Read:
+                        Read.Add(scope);
+                        break;
+                    case GitHubPermission.Write:
+                        Write.Add(scope);
+                        break;
+                    default:
+                        Read.Remove(scope);
+                        Write.Remove(scope);
+                        break;
+                }
+            }
+            return this;
+        }
+
+    }
+}
