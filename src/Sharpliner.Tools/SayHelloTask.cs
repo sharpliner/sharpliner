d1deef5758190e80c742d929f66144b480874aa0
Premek Vysoky <premek.vysoky@microsoft.com>
2021-05-05 12:33:12 +0200

    Add Sharpliner.tools and consume the NuGet in Sharpliner.CI

---
+++ src/Sharpliner.Tools/SayHelloTask.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Build.Framework;
+
+namespace Sharpliner.Tools
+{
+    public class SayHello : Microsoft.Build.Utilities.Task
+    {
+        public override bool Execute()
+        {
+            Log.LogMessage(MessageImportance.High, "Aloha *&%*@&#$^%(#@!^%#@*&^(%$*#@^%@(@#^*");
+            return true;
+        }
+    }
+}
