07dea167fbe4de10467808a7e9c88fe5efcd0575
Premek Vysoky <premek.vysoky@microsoft.com>
2021-05-05 10:03:04 +0200

    Add the Sharpliner.CI project and define the CI pipeline

---
+++ eng/Sharpliner.CI/SharplinerPipeline.cs
@@ -0,0 +1,71 @@
+﻿using Sharpliner.AzureDevOps;
+using Sharpliner.AzureDevOps.Tasks;
+
+namespace Sharpliner.CI
+{
+    internal class SharplinerPipeline : AzureDevOpsPipelineDefinition
+    {
+        public override string TargetFile => "azure-pipelines.yml";
+
+        public override TargetPathType TargetPathType => TargetPathType.RelativeToGitRoot;
+
+        public override AzureDevOpsPipeline Pipeline => new()
+        {
+            Trigger = new DetailedTrigger
+            {
+                Batch = true,
+                Branches = new()
+                {
+                    Include = { "main" }
+                }
+            },
+
+            Pr = new BranchPrTrigger("main"),
+
+            Stages =
+            {
+                new Stage("Build", "Build")
+                {
+                    Jobs =
+                    {
+                        new Job("Build", "Build and test")
+                        {
+                            Pool = new HostedPool("Azure Pipelines", "vs2017-win2016"),
+                            Steps =
+                            {
+                                new AzureDevOpsTask("UseDotNet@2", "Use .NET 5")
+                                {
+                                    Inputs = new TaskInputs
+                                    {
+                                        { "packageType", "sdk" },
+                                        { "version", "5.0.102" },
+                                    }
+                                },
+
+                                new AzureDevOpsTask("DotNetCoreCLI@2", "dotnet restore")
+                                {
+                                    Inputs = new TaskInputs
+                                    {
+                                        { "command", "restore" },
+                                        { "includeNuGetOrg", true },
+                                        { "projects", "Sharpliner.sln" },
+                                    }
+                                },
+
+                                new AzureDevOpsTask("DotNetCoreCLI@2", "dotnet test")
+                                {
+                                    Inputs = new TaskInputs
+                                    {
+                                        { "command", "test" },
+                                        { "includeNuGetOrg", true },
+                                        { "projects", "Sharpliner.sln" },
+                                    }
+                                },
+                            }
+                        }
+                    }
+                },
+            }
+        };
+    }
+}
