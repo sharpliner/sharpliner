cecde37cebc3a85bd97268c28d8e278d3c10015f
Premek Vysoky <premek.vysoky@microsoft.com>
2021-05-26 23:15:56 +0200

    Add half of resources

---
+++ src/Sharpliner/AzureDevOps/PipelineResource.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using YamlDotNet.Serialization;
+
+namespace Sharpliner.AzureDevOps
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/azure/devops/pipelines/yaml-schema?view=azure-devops&tabs=schema%2Cparameter-schema#pipeline-resource
+    /// </summary>
+    public record PipelineResource
+    {
+        /// <summary>
+        /// Identifier for the resource used in pipeline resource variables
+        /// </summary>
+        [YamlMember(Alias = "pipeline")]
+        public string Identifier { get; }
+
+        /// <summary>
+        /// Project for the source
+        /// Optional for current project
+        /// </summary>
+        [DisallowNull]
+        public string? Project { get; init; }
+
+        /// <summary>
+        /// Name of the pipeline that produces an artifact
+        /// </summary>
+        [DisallowNull]
+        public string? Source { get; init; }
+
+        /// <summary>
+        /// The pipeline run number to pick the artifact
+        /// Defaults to latest pipeline successful across all stages
+        /// Used only for manual or scheduled triggers
+        /// </summary>
+        [DisallowNull]
+        public string? Version { get; init; }
+
+        /// <summary>
+        /// Branch to pick the artifact
+        /// Optional, defaults to all branches
+        /// Used only for manual or scheduled triggers
+        /// </summary>
+        [DisallowNull]
+        public string? Branch { get; init; }
+
+        /// <summary>
+        /// List of tags required on the pipeline to pickup default artifacts
+        /// Optional, used only for manual or scheduled triggers
+        /// </summary>
+        public List<string> Tags { get; init; } = new();
+
+        /// <summary>
+        /// Triggers are not enabled by default unless you add trigger section to the resource
+        /// </summary>
+        [DisallowNull]
+        public PipelineTrigger? Trigger { get; init; }
+
+        public PipelineResource(string identifier)
+        {
+            Identifier = identifier ?? throw new System.ArgumentNullException(nameof(identifier));
+        }
+    }
+}
--- src/Sharpliner/AzureDevOps/Resources.cs
+++ src/Sharpliner/AzureDevOps/Resources.cs
@@ -1,7 +1,22 @@
-﻿namespace Sharpliner.AzureDevOps
+﻿using Sharpliner.ConditionedDefinitions;
+
+namespace Sharpliner.AzureDevOps
 {
-    public record Resources(
-        object? Pipelines,     // TODO
-        object? Containers,    // TODO
-        object? Repositories); // TODO
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/azure/devops/pipelines/yaml-schema?view=azure-devops&tabs=schema%2Cparameter-schema#resources
+    /// </summary>
+    public record Resources
+    {
+        public ConditionedDefinitionList<ConditionedDefinition<PipelineResource>> Pipelines { get; init; } = new();
+
+        /* TODO
+        public ConditionedDefinitionList<ConditionedDefinition<BuildResource>> Builds { get; init; } = new();
+
+        public ConditionedDefinitionList<ConditionedDefinition<RepositoryResource>> Repositories { get; init; } = new();
+        */
+
+        public ConditionedDefinitionList<ConditionedDefinition<ContainerResource>> Containers { get; init; } = new();
+
+        public ConditionedDefinitionList<ConditionedDefinition<PackageResource>> Packages { get; init; } = new();
+    }
 }
