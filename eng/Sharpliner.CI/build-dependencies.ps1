445b2b89507ab873d9ab448c58ffbe1a5f302293
Premek Vysoky <premek.vysoky@microsoft.com>
2021-07-24 11:12:53 +0200

    Use version 42.42.42 locally

--- eng/Sharpliner.CI/build-dependencies.ps1
+++ eng/Sharpliner.CI/build-dependencies.ps1
@@ -9,12 +9,12 @@ if (-not(Test-Path "$repo_root/artifacts/packages")) {
     New-Item -Path "$repo_root/artifacts" -Name "packages" -ItemType "directory" | out-null
 }
 
-if (-not(Test-Path "$repo_root/artifacts/packages/Sharpliner.1.0.0.nupkg")) {
+if (-not(Test-Path "$repo_root/artifacts/packages/Sharpliner.42.42.42.nupkg")) {
     Write-Host "Building Sharpliner nupkg for Sharpliner.CI..."
-    dotnet pack --nologo "$repo_root/src/Sharpliner/Sharpliner.csproj"
+    dotnet pack --nologo "$repo_root/src/Sharpliner/Sharpliner.csproj" -p:PackageVersion=42.42.42.42
 }
 
-if (-not(Test-Path "$repo_root/artifacts/packages/Sharpliner.Tools.1.0.0.nupkg")) {
+if (-not(Test-Path "$repo_root/artifacts/packages/Sharpliner.Tools.42.42.42.nupkg")) {
     Write-Host "Building Sharpliner.Tools nupkg for Sharpliner.CI..."
-    dotnet pack --nologo "$repo_root/src/Sharpliner.Tools/Sharpliner.Tools.csproj"
+    dotnet pack --nologo "$repo_root/src/Sharpliner.Tools/Sharpliner.Tools.csproj" -p:PackageVersion=42.42.42.42
 }
