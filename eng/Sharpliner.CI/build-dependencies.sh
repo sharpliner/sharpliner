0bfc3b0880d1d05cd8cf57086d999990940dab2c
Premek Vysoky <premek.vysoky@microsoft.com>
2021-08-21 15:16:51 +0200

    Merge Sharpliner.Tools into Sharpliner

--- eng/Sharpliner.CI/build-dependencies.sh
+++ eng/Sharpliner.CI/build-dependencies.sh
@@ -21,9 +21,3 @@ if [ ! -f "$repo_root/artifacts/packages/Sharpliner.42.42.42.nupkg" ]; then
   echo "Building Sharpliner nupkg for Sharpliner.CI..."
   dotnet pack --nologo "$repo_root/src/Sharpliner/Sharpliner.csproj" -p:PackageVersion=42.42.42
 fi
-
-if [ ! -f "$repo_root/artifacts/packages/Sharpliner.Tools.42.42.42.nupkg" ]; then
-  mkdir -p "$repo_root/artifacts/packages"
-  echo "Building Sharpliner.Tools nupkg for Sharpliner.CI..."
-  dotnet pack --nologo "$repo_root/src/Sharpliner.Tools/Sharpliner.Tools.csproj" -p:PackageVersion=42.42.42
-fi
