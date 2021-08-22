159f6d691fd990098ded5d4818f65b1bbbc2f1b5
Premek Vysoky <premek.vysoky@microsoft.com>
2021-08-22 11:22:03 +0200

    Revert test changes

--- eng/Sharpliner.CI/PublishPipeline.cs
+++ eng/Sharpliner.CI/PublishPipeline.cs
@@ -11,33 +11,42 @@ namespace Sharpliner.CI
 
         public override SingleStageAzureDevOpsPipeline Pipeline => new()
         {
-
-            Pr = new PrTrigger("main"),
-
-            Variables =
+            Jobs =
             {
-                If.IsBranch("net-6.0")
-                    .Variable("DotnetVersion", "6.0.100")
-                    .Group("net6-kv")
-                .Else
-                    .Variable("DotnetVersion", "5.0.202"),
-            },
-
-                Jobs =
-            {
-                new Job("Build", "Build and test")
+                new Job("Publish", "Publish to nuget.org")
                 {
                     Pool = new HostedPool("Azure Pipelines", "windows-latest"),
                     Steps =
                     {
-                        If.IsPullRequest
-                            .Step(Powershell.Inline("Write-Host 'Hello-World'").DisplayAs("Hello world")),
+                        Powershell.FromResourceFile("Get-Version.ps1").DisplayAs("Detect package version"),
 
-                        DotNet.Install(DotNetPackageType.Sdk, "$(DotnetVersion)").DisplayAs("Install .NET SDK"),
+                        DotNet.Install(DotNetPackageType.Sdk, "6.0.100-preview.3.21202.5").DisplayAs("Install .NET 6 preview 3"),
 
-                        DotNet.Build("src/MyProject.sln", includeNuGetOrg: true).DisplayAs("Build"),
+                        Powershell.Inline("New-Item -Path 'artifacts' -Name 'packages' -ItemType 'directory'"),
 
-                        DotNet.Command(DotNetCommand.Test, projects: "src/MyProject.sln").DisplayAs("Test"),
+                        DotNet.Build("src/Sharpliner/Sharpliner.csproj", arguments: "-c Release", includeNuGetOrg: true).DisplayAs("Build"),
+
+                        DotNet
+                            .Custom("pack", arguments:
+                                "src/Sharpliner/Sharpliner.csproj " +
+                                "-c Release --output artifacts/packages " +
+                                "-p:PackageVersion=$(majorVersion).$(minorVersion).$(patchVersion)")
+                            .DisplayAs("Pack the .nupkg"),
+
+                        Publish("Sharpliner", "artifacts/packages/Sharpliner.$(majorVersion).$(minorVersion).$(patchVersion).nupkg", "Publish build artifacts"),
+
+                        If.And(IsNotPullRequest, IsBranch("refs/heads/main"))
+                            .Step(Task("NuGetAuthenticate@0", "Authenticate NuGet"))
+                            .Step(Task("NuGetCommand@2", "Publish to nuget.org") with
+                            {
+                                Inputs = new()
+                                {
+                                    { "command", "push" },
+                                    { "packagesToPush", "artifacts/packages/Sharpliner.$(majorVersion).$(minorVersion).$(patchVersion).nupkg" },
+                                    { "nuGetFeedType", "external" },
+                                    { "publishFeedCredentials", "Sharpliner / nuget.org" },
+                                }
+                            })
                     }
                 }
             },
