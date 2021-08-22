79fe54b4d2f2866144ef687fdf96ff873039d365
Premek Vysoky <premek.vysoky@microsoft.com>
2021-08-22 13:42:53 +0200

    Move AzDO specific code to AzDO namespace

--- src/Sharpliner/AzureDevOps/AzureDevOpsPipelineDefinition.cs
+++ src/Sharpliner/AzureDevOps/AzureDevOpsPipelineDefinition.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Text.RegularExpressions;
 using Sharpliner.AzureDevOps.Tasks;
 using Sharpliner.Definition;
 
@@ -12,17 +10,28 @@ namespace Sharpliner.AzureDevOps
         /// </summary>
         public abstract TPipeline Pipeline { get; }
 
+        public override void Validate() => Pipeline.Validate();
+
+        public override string Serialize() => PrettifyYaml(SharplinerSerializer.Serialize(Pipeline));
+
+        #region Syntax sugar for common AzDO pipelines elements
+
+        protected static ConditionBuilder If => new();
+
         protected static ConditionedDefinition<VariableBase> Template(string path)
             => new Template<VariableBase>(path);
         protected static ConditionedDefinition<T> Template<T>(string path, TemplateParameters? parameters = null)
             => new Template<T>(path, parameters);
 
-        protected static ConditionBuilder If => new();
-
+        /// <summary>
+        /// Allows the variables[""] notation for conditional definitions.
+        /// </summary>
+        protected readonly PipelineVariable variables = new();
         protected static ConditionedDefinition<VariableBase> Variable(string name, string value) => new(new Variable(name, value));
         protected static ConditionedDefinition<VariableBase> Variable(string name, int value) => new(new Variable(name, value));
         protected static ConditionedDefinition<VariableBase> Variable(string name, bool value) => new(new Variable(name, value));
         protected static ConditionedDefinition<VariableBase> Group(string name) => new(new VariableGroup(name));
+
         protected static BashTaskBuilder Bash { get; } = new();
         protected static ScriptTaskBuilder Script { get; } = new();
         protected static PowershellTaskBuilder Powershell { get; } = new(false);
@@ -39,20 +48,31 @@ namespace Sharpliner.AzureDevOps
         protected static Job Job(string jobName, string? displayName = null) => new Job(jobName) with { DisplayName = displayName! };
         protected static DotNetTaskBuilder DotNet { get; } = new();
 
-        public override void Validate() => Pipeline.Validate();
+        protected static Condition<T> And<T>(Condition condition1, Condition condition2) => new AndCondition<T>(condition1, condition2);
 
-        public override string Serialize() => Prettify(SharplinerSerializer.Serialize(Pipeline));
+        protected static Condition Or<T>(Condition condition1, Condition condition2) => new OrCondition<T>(condition1, condition2);
 
-        private static string Prettify(string yaml)
-        {
-            // Add empty new lines to make text more readable
-            yaml = Regex.Replace(yaml, "((\r?\n)[a-zA-Z]+:)", Environment.NewLine + "$1");
-            yaml = Regex.Replace(yaml, "((\r?\n) {0,8}- ?[a-zA-Z]+@?[a-zA-Z\\.0-9]*:)", Environment.NewLine + "$1");
-            yaml = Regex.Replace(yaml, "((\r?\n) {0,8}- ?\\${{ ?if[^\n]+\n)", Environment.NewLine + "$1");
-            yaml = Regex.Replace(yaml, "(:\r?\n\r?\n)", ":" + Environment.NewLine);
+        protected static Condition<T> Equal<T>(string expression1, string expression2) => new EqualityCondition<T>(expression1, expression2, true);
 
-            return yaml;
-        }
+        protected static Condition<T> NotEqual<T>(string expression1, string expression2) => new EqualityCondition<T>(expression1, expression2, false);
+
+        protected static Condition And(Condition condition1, Condition condition2) => new AndCondition(condition1, condition2);
+
+        protected static Condition Or(Condition condition1, Condition condition2) => new OrCondition(condition1, condition2);
+
+        protected static Condition Equal(string expression1, string expression2) => new EqualityCondition(expression1, expression2, true);
+
+        protected static Condition NotEqual(string expression1, string expression2) => new EqualityCondition(expression1, expression2, false);
+
+        protected static Condition IsBranch(string branchName) => new BranchCondition(branchName, true);
+
+        protected static Condition IsNotBranch(string branchName) => new BranchCondition(branchName, false);
+
+        protected static Condition IsPullRequest => new BuildReasonCondition("'PullRequest'", true);
+
+        protected static Condition IsNotPullRequest => new BuildReasonCondition("'PullRequest'", false);
+
+        #endregion
     }
 
     public abstract class AzureDevOpsPipelineDefinition : AzureDevOpsPipelineDefinitionBase<AzureDevOpsPipeline>
