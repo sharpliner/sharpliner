a45a4823dba0ee4cd675304f5b4f830d1b6e954c
Premek Vysoky <premek.vysoky@microsoft.com>
2021-10-12 10:53:20 +0200

    Mark the whole tree as list

--- src/Sharpliner/AzureDevOps/ConditionedDefinitionList.cs
+++ src/Sharpliner/AzureDevOps/ConditionedDefinitionList.cs
@@ -9,17 +9,35 @@ namespace Sharpliner.AzureDevOps
     {
         public new void Add(ConditionedDefinition<T> item)
         {
-            item.IsList = true;
-
             // When we define a tree of conditional definitions, the expression returns
             // the leaf definition so we have to move up to the top-level definition
-            while (item.Parent != null)
+            while (item.Parent is ConditionedDefinition<T> parent)
             {
-                item = (T)item.Parent;
-                item.IsList = true;
+                item = parent;
             }
 
+            SetIsListForAll(item);
             base.Add(item);
+        }
+
+        private static void SetIsListForAll(ConditionedDefinition? item)
+        {
+            if (item == null)
+            {
+                return;
+            }
+
+            item.IsList = true;
+
+            if (item is ConditionedDefinition<T> temp && temp.Definition is ConditionedDefinition temp2)
+            {
+                SetIsListForAll(temp2);
+            }
+
+            foreach (var child in item.Definitions)
+            {
+                SetIsListForAll(child);
+            }
         }
     }
 }
