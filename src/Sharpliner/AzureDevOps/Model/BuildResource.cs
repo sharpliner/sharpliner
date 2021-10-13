a5e549e35038171cfbb51447aadbfbb309c95f0d
Premek Vysoky <premek.vysoky@microsoft.com>
2021-10-13 14:36:52 +0200

    Complete Resources model

---
+++ src/Sharpliner/AzureDevOps/Model/BuildResource.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using YamlDotNet.Serialization;
+
+namespace Sharpliner.AzureDevOps
+{
+    /// <summary>
+    /// If you have an external CI build system that produces artifacts, you can consume artifacts with a builds resource.
+    /// A builds resource can be any external CI systems like Jenkins, TeamCity, CircleCI, and so on.
+    /// More details can be found in <see href="https://docs.microsoft.com/en-us/azure/devops/pipelines/process/resources?view=azure-devops&tabs=schema#define-a-builds-resource">official Azure DevOps pipelines documentation</see>.
+    /// </summary>
+    public record BuildResource
+    {
+        /// <summary>
+        /// Identifier for the resource used in build resource variables
+        /// </summary>
+        [YamlMember(Alias = "build")]
+        public string Identifier { get; }
+
+        /// <summary>
+        /// The type of your build service like Jenkins, circleCI etc.
+        /// </summary>
+        [DisallowNull]
+        public string? Type { get; init; }
+
+        /// <summary>
+        /// Service connection for your build service
+        /// </summary>
+        [DisallowNull]
+        public string? Connection { get; init; }
+
+        /// <summary>
+        /// Source definition of the build
+        /// </summary>
+        [DisallowNull]
+        public string? Source { get; init; }
+
+        /// <summary>
+        /// The build number to pick the artifact, defaults to latest successful build
+        /// </summary>
+        [DisallowNull]
+        public string? Version { get; init; }
+
+        /// <summary>
+        /// Triggers aren't enabled by default and should be explicitly set
+        /// </summary>
+        [DisallowNull]
+        public bool Trigger { get; init; } = false;
+
+        public BuildResource(string identifier)
+        {
+            Identifier = identifier ?? throw new System.ArgumentNullException(nameof(identifier));
+        }
+    }
+}
--- src/Sharpliner/AzureDevOps/Model/Resources.cs
+++ src/Sharpliner/AzureDevOps/Model/Resources.cs
@@ -1,20 +1,20 @@
 ﻿namespace Sharpliner.AzureDevOps
 {
     /// <summary>
-    /// More details can be found in <see href="https://docs.microsoft.com/en-us/azure/devops/pipelines/yaml-schema?view=azure-devops&amp;tabs=schema%2Cparameter-schema#resources">official Azure DevOps pipelines documentation</see>.
+    /// More details can be found in <see href="https://docs.microsoft.com/en-us/azure/devops/pipelines/process/resources?view=azure-devops&tabs=schema">official Azure DevOps pipelines documentation</see>.
     /// </summary>
     public record Resources
     {
         public ConditionedList<PipelineResource> Pipelines { get; init; } = new();
 
-        /* TODO
-        public ConditionedDefinitionList<ConditionedDefinition<BuildResource>> Builds { get; init; } = new();
+        public ConditionedList<BuildResource> Builds { get; init; } = new();
 
-        public ConditionedDefinitionList<ConditionedDefinition<RepositoryResource>> Repositories { get; init; } = new();
-        */
+        public ConditionedList<RepositoryResource> Repositories { get; init; } = new();
 
         public ConditionedList<ContainerResource> Containers { get; init; } = new();
 
         public ConditionedList<PackageResource> Packages { get; init; } = new();
+
+        public ConditionedList<WebhookResource> Webhooks { get; init; } = new();
     }
 }
