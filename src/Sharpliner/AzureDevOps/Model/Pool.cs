8c18d986bfd8cfe67c67e214ec521b83489d9ea9
Premek Vysoky <premek.vysoky@microsoft.com>
2021-10-13 12:17:52 +0200

    Complete the Pool model

--- src/Sharpliner/AzureDevOps/Model/Pool.cs
+++ src/Sharpliner/AzureDevOps/Model/Pool.cs
@@ -1,9 +1,37 @@
-﻿namespace Sharpliner.AzureDevOps
-{
-    public record Pool(string? Name = null);
+﻿using System.Diagnostics.CodeAnalysis;
+using YamlDotNet.Serialization;
 
-    public record HostedPool(string? Name = null, string? VmImage = null) : Pool(Name)
+namespace Sharpliner.AzureDevOps
+{
+    public record Pool
     {
+        /// <summary>
+        /// Identifier for this step (A-Z, a-z, 0-9, and underscore).
+        /// </summary>
+        [YamlMember(Order = 100)]
+        public string? Name { get; init; }
+
+        public Pool(string? name)
+        {
+            Name = name;
+        }
+    }
+
+    public record HostedPool : Pool
+    {
+        public HostedPool(string? name = null, string? vmImage = null) : base(name)
+        {
+            VmImage = vmImage;
+        }
+
+        /// <summary>
+        /// Identifier for this step (A-Z, a-z, 0-9, and underscore).
+        /// </summary>
+        [YamlMember(Order = 105)]
+        public string? VmImage { get; init; }
+
+        [YamlMember(Order = 110)]
+        [DisallowNull]
         public ConditionedList<string> Demands { get; init; } = new();
     }
 }
