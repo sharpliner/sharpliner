a8453000457dfae270c25aa87ba494f2f24ba695
Premek Vysoky <premek.vysoky@microsoft.com>
2021-10-15 10:19:01 +0200

    Fix nullability warning in Servie.cs

--- src/Sharpliner/GitHubActions/Service.cs
+++ src/Sharpliner/GitHubActions/Service.cs
@@ -8,28 +8,6 @@ namespace Sharpliner.GitHubActions
     public record Service
     {
         /// <summary>
-        /// The service with the given Id will be used for the the job.
-        /// </summary>
-        /// <param name="id">Id of the service.</param>
-        /// <exception cref="ArgumentNullException">Raised if the service Id is null or empty.</exception>
-        public Service(string id)
-        {
-            if (string.IsNullOrEmpty(id))
-                throw new ArgumentNullException((nameof(id)));
-            Id = id;
-        }
-
-        /// <summary>
-        /// The service with the given Id and container will be used for the job.
-        /// </summary>
-        /// <param name="id">Id of the service.</param>
-        /// <param name="container">Container of the service.</param>
-        public Service(string id, Container container) : this(id)
-        {
-            Container = container;
-        }
-
-        /// <summary>
         /// Get the Id of the service.
         /// </summary>
         public string Id { get; }
@@ -37,6 +15,22 @@ namespace Sharpliner.GitHubActions
         /// <summary>
         /// Get/Set the container of the service.
         /// </summary>
-        public Container Container { get; set; }
+        public Container? Container { get; set; }
+
+        /// <summary>
+        /// The service with the given Id and container will be used for the job.
+        /// </summary>
+        /// <param name="id">Id of the service.</param>
+        /// <param name="container">Container of the service.</param>
+        public Service(string id, Container? container = null)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+
+            Id = id;
+            Container = container;
+        }
     }
 }
