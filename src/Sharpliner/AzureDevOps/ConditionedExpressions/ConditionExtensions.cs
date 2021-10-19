fe764971989650dab77d28e5788095dabd843f1b
Premek Vysoky <premek.vysoky@microsoft.com>
2021-10-19 19:52:29 +0200

    Fix build

--- src/Sharpliner/AzureDevOps/ConditionedExpressions/ConditionExtensions.cs
+++ src/Sharpliner/AzureDevOps/ConditionedExpressions/ConditionExtensions.cs
@@ -16,17 +16,35 @@ namespace Sharpliner.AzureDevOps
         public static Conditioned<VariableBase> Group(this Condition condition, string name)
             => Conditioned.Link<VariableBase>(condition, new VariableGroup(name));
 
+        public static Conditioned<Stage> Stage(this Condition condition, Stage stage)
+            => Conditioned.Link(condition, stage);
+
         public static Conditioned<Stage> Stage(this Condition condition, Conditioned<Stage> stage)
             => Conditioned.Link(condition, stage);
 
+        public static Conditioned<JobBase> Job(this Condition condition, JobBase job)
+            => Conditioned.Link(condition, job);
+
         public static Conditioned<JobBase> Job(this Condition condition, Conditioned<JobBase> job)
+            => Conditioned.Link(condition, job);
+
+        public static Conditioned<JobBase> DeploymentJob(this Condition condition, JobBase job)
             => Conditioned.Link(condition, job);
 
         public static Conditioned<JobBase> DeploymentJob(this Condition condition, Conditioned<JobBase> job)
             => Conditioned.Link(condition, job);
 
+        public static Conditioned<Step> Step(this Condition condition, Step step)
+            => Conditioned.Link(condition, step);
+
         public static Conditioned<Step> Step(this Condition condition, Conditioned<Step> step)
             => Conditioned.Link(condition, step);
+
+        public static Conditioned<Pool> Pool(this Condition condition, Pool pool)
+            => Conditioned.Link(condition, pool);
+
+        public static Conditioned<Pool> Pool(this Condition condition, Conditioned<Pool> pool)
+            => Conditioned.Link(condition, pool);
 
         public static Conditioned<T> Template<T>(this Condition condition, string path, TemplateParameters? parameters = null)
             => Conditioned.Link(condition, new Template<T>(condition: condition.ToString(), path: path, parameters ?? new TemplateParameters()));
