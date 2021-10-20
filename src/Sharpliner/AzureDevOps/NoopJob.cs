068c2385b1f2af40f49a4bda37532e6fb7790359
Premek Vysoky <premek.vysoky@microsoft.com>
2021-10-20 15:56:54 +0200

    [AzureDevops] Add the server pool and the noop job

---
+++ src/Sharpliner/AzureDevOps/NoopJob.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+using YamlDotNet.Serialization;
+
+namespace Sharpliner.AzureDevOps
+{
+    /// <summary>
+    /// No-op job is used when other jobs might be omitted based on a condition and no job is left in the stage.
+    /// </summary>
+    public record NoopJob : Job, IYamlConvertible
+    {
+        public NoopJob() : base("No_op")
+        {
+        }
+
+        public void Read(IParser parser, Type expectedType, ObjectDeserializer nestedObjectDeserializer) => throw new NotImplementedException();
+        public void Write(IEmitter emitter, ObjectSerializer nestedObjectSerializer)
+        {
+            emitter.Emit(new MappingStart());
+            emitter.Emit(new Scalar("job"));
+            emitter.Emit(new Scalar("No_op"));
+            emitter.Emit(new MappingEnd());
+        }
+    }
+}
