921545d9600ced421eaa0f100d0210b2bf8ff92d
Přemek Vysoký <premek.vysoky@microsoft.com>
2021-10-23 19:44:40 +0200

    [AzureDevOps] Add the no_op job (#117)

---
+++ tests/Sharpliner.Tests/AzureDevOps/Model/NoopJobTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Sharpliner.AzureDevOps;
+using Xunit;
+
+namespace Sharpliner.Tests.AzureDevOps;
+
+public class NoopJobTests
+{
+    private class NoopJobPipeline : SimpleTestPipeline
+    {
+        public override SingleStagePipeline Pipeline => new()
+        {
+            Jobs =
+            {
+                new NoopJob
+                {
+                    Pool = new HostedPool(vmImage: "windows-2019")
+                }
+            }
+        };
+    }
+
+    [Fact]
+    public void Serialize_Pipeline_Test()
+    {
+        NoopJobPipeline pipeline = new();
+        string yaml = pipeline.Serialize();
+        yaml.Should().Be(
+@"jobs:
+- job: No_op
+  pool:
+    vmImage: windows-2019
+");
+    }
+}
