b1c59ef67c5aba3e29ab36b96374227b30a94512
Premek Vysoky <premek.vysoky@microsoft.com>
2021-11-17 10:51:52 +0100

    Disallow new Template<T>()

---
+++ src/Sharpliner/AzureDevOps/TemplateParameters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+using YamlDotNet.Serialization;
+
+namespace Sharpliner.AzureDevOps;
+
+/// <summary>
+/// This class allows you to pass parameters to to templates.
+/// To nest objects, insert another TemplateParameters value.
+/// You can also specify a condition in the key and nest values conditionally.
+/// </summary>
+public class TemplateParameters : Dictionary<string, object>, IYamlConvertible
+{
+    public void Read(IParser parser, Type expectedType, ObjectDeserializer nestedObjectDeserializer) => throw new NotImplementedException();
+
+    public void Write(IEmitter emitter, ObjectSerializer nestedObjectSerializer)
+    {
+        emitter.Emit(new MappingStart());
+
+        foreach (var parameter in this)
+        {
+            emitter.Emit(new Scalar(parameter.Key));
+            nestedObjectSerializer(parameter.Value);
+        }
+
+        emitter.Emit(new MappingEnd());
+    }
+}
