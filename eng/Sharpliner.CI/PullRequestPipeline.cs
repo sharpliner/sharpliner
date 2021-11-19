bea1f603f61ef0214111dfb2c86dbea1709efad4
Premek Vysoky <premek.vysoky@microsoft.com>
2021-11-19 09:22:58 +0100

    Make unpublished change

--- eng/Sharpliner.CI/PullRequestPipeline.cs
+++ eng/Sharpliner.CI/PullRequestPipeline.cs
@@ -19,7 +19,7 @@ class PullRequestPipeline : SingleStagePipelineDefinition
 
         Jobs =
         {
-            new Job("Build", "Build and test")
+            new Job("Build", "Build and test TODO")
             {
                 Pool = new HostedPool("Azure Pipelines", "windows-latest"),
                 Steps =
