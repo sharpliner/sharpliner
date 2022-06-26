62ace1c042284d493b6825d8fb0d3eeb3d7ee0f3
Premek Vysoky <premek.vysoky@microsoft.com>
2022-06-26 17:17:10 +0200

    Move Job.Steps as last serialized property

--- src/Sharpliner/AzureDevOps/Model/Job.cs
+++ src/Sharpliner/AzureDevOps/Model/Job.cs
@@ -29,18 +29,18 @@ public record Job : JobBase
     public Conditioned<Strategy>? Strategy { get => _strategy; init => _strategy = value?.GetRoot(); }
 
     /// <summary>
-    /// A step is a linear sequence of operations that make up a job
-    /// Each step runs in its own process on an agent and has access to the pipeline workspace on a local hard drive.
-    /// This behavior means environment variables aren't preserved between steps but file system changes are.
-    /// </summary>
-    [YamlMember(Order = 700)]
-    public ConditionedList<Step> Steps { get; init; } = new();
-
-    /// <summary>
     /// Any resources (repos or pools) required by this job that are not already referenced.
     /// </summary>
     [YamlMember(Order = 1200)]
     public Uses? Uses { get; init; }
+
+    /// <summary>
+    /// A step is a linear sequence of operations that make up a job
+    /// Each step runs in its own process on an agent and has access to the pipeline workspace on a local hard drive.
+    /// This behavior means environment variables aren't preserved between steps but file system changes are.
+    /// </summary>
+    [YamlMember(Order = 1300)]
+    public ConditionedList<Step> Steps { get; init; } = new();
 
     /// <summary>
     /// A job is a collection of steps run by an agent or on a server.
