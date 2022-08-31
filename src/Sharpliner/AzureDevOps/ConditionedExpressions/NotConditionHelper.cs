08d392cb119a8e91f11cb7b465ffb18221e0a7bc
Přemek Vysoký <premek.vysoky@microsoft.com>
2022-08-31 09:50:52 +0200

    Strip quoted expressions inside conditions (#231)

---
+++ src/Sharpliner/AzureDevOps/ConditionedExpressions/NotConditionHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Sharpliner.AzureDevOps.ConditionedExpressions;
+
+internal static class NotConditionHelper
+{
+    public static string NegateCondition(string condition)
+    {
+        if (condition.StartsWith("eq("))
+        {
+            return string.Concat("ne", condition.AsSpan(2));
+        }
+
+        if (condition.StartsWith("ne("))
+        {
+            return string.Concat("eq", condition.AsSpan(2));
+        }
+
+        if (condition.StartsWith("in("))
+        {
+            return string.Concat("notIn", condition.AsSpan(2));
+        }
+
+        if (condition.StartsWith("notIn("))
+        {
+            return string.Concat("in", condition.AsSpan(5));
+        }
+
+        return $"not({condition})";
+    }
+}
