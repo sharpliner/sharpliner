48c782de9ed94ab21d69d30fcb5740076d8e4978
Premek Vysoky <premek.vysoky@microsoft.com>
2022-09-23 10:50:57 +0200

    Add entrypoint for GitHub workflow definition

---
+++ src/Sharpliner/GitHubActions/WorkflowDefinition.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Sharpliner.Common;
+
+namespace Sharpliner.GitHubActions;
+
+// TODO (GitHub Actions): Made internal until we get to a more complete API
+/// <summary>
+/// Inherit from this class to define a GitHub workflow.
+/// </summary>
+internal abstract class WorkflowDefinition : ISharplinerDefinition
+{
+    /// <summary>
+    /// Path to the YAML file where this pipeline will be exported to.
+    /// When you build the project, the pipeline will be saved into a file on this path.
+    /// Example: "pipelines/ci.yaml"
+    /// </summary>
+    public abstract string TargetFile { get; }
+
+    public virtual TargetPathType TargetPathType => TargetPathType.RelativeToCurrentDir;
+
+    /// <summary>
+    /// Header that will be shown at the top of the generated YAML file
+    /// 
+    /// Leave empty array to omit file header.
+    /// </summary>
+    public virtual string[]? Header => SharplinerPublisher.GetDefaultHeader(GetType());
+
+    /// <summary>
+    /// Define the pipeline by implementing this field.
+    /// </summary>
+    public abstract Workflow Workflow { get; }
+
+    // TODO: Add validations for workflows
+    /// <summary>
+    /// Returns the list of validations that should be run on the definition (e.g. wrong dependsOn, artifact name typos..).
+    /// </summary>
+    public IReadOnlyCollection<IDefinitionValidation> Validations => Array.Empty<IDefinitionValidation>();
+
+    /// <summary>
+    /// Serializes this pipeline into YAML.
+    /// </summary>
+    public string Serialize() => SharplinerSerializer.Serialize(Workflow);
+}
