5207e04e6401cb614820ee44937d0ad24e212a9c
Premek Vysoky <premek.vysoky@microsoft.com>
2022-10-25 23:58:09 +0200

    Export API

--- src/Sharpliner/AzureDevOps/StaticVariablesReference.cs
+++
@@ -1,8 +0,0 @@
-﻿using Sharpliner.AzureDevOps.ConditionedExpressions;
-
-namespace Sharpliner.AzureDevOps;
-
-public class StaticVariablesReference : VariablesReference
-{
-    public new VariableReference this[string variableName] => new(variableName);
-}
