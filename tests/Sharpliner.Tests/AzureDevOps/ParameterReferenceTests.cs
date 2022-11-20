38c5518d3dcc7391cdbd8d655bea2aa43d0b2c01
Premek Vysoky <premek.vysoky@microsoft.com>
2022-11-20 10:51:49 +0100

    Add the target unit test

---
+++ tests/Sharpliner.Tests/AzureDevOps/ParameterReferenceTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Sharpliner.AzureDevOps;
+using Sharpliner.AzureDevOps.ConditionedExpressions;
+
+namespace Sharpliner.Tests.AzureDevOps;
+
+public class ParameterReferenceTests
+{
+    private class RunOnWindowsTemplate : StepTemplateDefinition
+    {
+        public override string TargetFile => "run-on-windows.yml";
+
+        public override List<Parameter> Parameters => new()
+        {
+            StringParameter("agentOs"),
+            StepListParameter("steps"),
+        };
+
+            public override ConditionedList<Step> Definition => new()
+        {
+            If.Equal(parameters["agentOs"], "Windows_NT")
+                .Step(parameters["steps"])
+        };
+    }
+}
