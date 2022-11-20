7cf329817c862c15f9a4e5849b2268d801cb1078
Přemek Vysoký <premek.vysoky@microsoft.com>
2022-11-20 11:33:06 +0100

    Allow using parameter reference anywhere (#257)

--- src/Sharpliner/AzureDevOps/ConditionedExpressions/ParameterReference.cs
+++ src/Sharpliner/AzureDevOps/ConditionedExpressions/ParameterReference.cs
@@ -6,6 +6,9 @@ using YamlDotNet.Serialization;
 
 namespace Sharpliner.AzureDevOps.ConditionedExpressions;
 
+/// <summary>
+/// Class that makes it possible to put ${{ parameters['foo'] }} everywhere.
+/// </summary>
 public class ParameterReference : IRuntimeExpression, ICompileTimeExpression, IYamlConvertible
 {
     public string ParameterName { get; }
@@ -28,4 +31,27 @@ public class ParameterReference : IRuntimeExpression, ICompileTimeExpression, IY
 
     public void Write(IEmitter emitter, ObjectSerializer nestedObjectSerializer)
         => emitter.Emit(new Scalar(ToString()));
+
+    public static implicit operator Conditioned<VariableBase>(ParameterReference value) => new ConditionedParameterReference<VariableBase>(value);
+
+    public static implicit operator Conditioned<Stage>(ParameterReference value) => new ConditionedParameterReference<Stage>(value);
+
+    public static implicit operator Conditioned<JobBase>(ParameterReference value) => new ConditionedParameterReference<JobBase>(value);
+
+    public static implicit operator Conditioned<Step>(ParameterReference value) => new ConditionedParameterReference<Step>(value);
+
+    public static implicit operator Conditioned<Pool>(ParameterReference value) => new ConditionedParameterReference<Pool>(value);
+}
+
+public record ConditionedParameterReference<T> : Conditioned<T>
+{
+    private readonly ParameterReference _parameter;
+
+    public ConditionedParameterReference(ParameterReference parameter) : base()
+    {
+        _parameter = parameter;
+    }
+
+    public override void Write(IEmitter emitter, ObjectSerializer nestedObjectSerializer)
+        => emitter.Emit(new Scalar(_parameter.CompileTimeExpression));
 }
---
+++ tests/Sharpliner.Tests/AzureDevOps/ParameterReferenceTests.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Sharpliner.AzureDevOps;
+using Sharpliner.AzureDevOps.ConditionedExpressions;
+using Xunit;
+
+namespace Sharpliner.Tests.AzureDevOps;
+
+public class ParameterReferenceTests
+{
+    private class ParameterReferenceTest_Template : StageTemplateDefinition
+    {
+        public override string TargetFile => "stages.yml";
+
+        public override List<Parameter> Parameters => new()
+        {
+            StageListParameter("stages"),
+            JobListParameter("jobs"),
+            StepListParameter("steps"),
+            ObjectParameter("variables"),
+            ObjectParameter("pool", new()
+            {
+                { "vmImage", "windows-latest" }
+            }),
+        };
+
+        public override ConditionedList<Stage> Definition => new()
+        {
+            new Stage("Stage_1")
+            {
+                Jobs =
+                {
+                    new Job("Job_1")
+                    {
+                        Pool = parameters["pool"],
+
+                        Steps =
+                        {
+                            If.Equal(parameters["agentOs"], "Windows_NT")
+                                .Step(parameters["steps"]),
+
+                            parameters["steps"],
+                        }
+                    },
+
+                    parameters["jobs"],
+                }
+            },
+
+            parameters["stages"],
+        };
+    }
+
+    // This tests that we can include parameters at any point of the pipeline
+    [Fact]
+    public void PipelineParameter_Serialization_Test()
+    {
+        var yaml = new ParameterReferenceTest_Template().Serialize();
+
+        yaml.Trim().Should().Be(
+            """
+            parameters:
+            - name: stages
+              type: stageList
+
+            - name: jobs
+              type: jobList
+
+            - name: steps
+              type: stepList
+
+            - name: variables
+              type: object
+
+            - name: pool
+              type: object
+              default:
+                vmImage: windows-latest
+
+            stages:
+            - stage: Stage_1
+              jobs:
+              - job: Job_1
+                pool: ${{ parameters.pool }}
+                steps:
+                - ${{ parameters.steps }}
+                - ${{ parameters.steps }}
+            
+              - ${{ parameters.jobs }}
+            - ${{ parameters.stages }}
+            """);
+    }
+}
