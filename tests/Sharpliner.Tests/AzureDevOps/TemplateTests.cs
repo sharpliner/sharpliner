c1ceec1298d8802a073344efabdca54596be796f
Premek Vysoky <premek.vysoky@microsoft.com>
2022-11-20 11:16:20 +0100

    Allow putting `${{ parameters.foo }}` anywhere

--- tests/Sharpliner.Tests/AzureDevOps/TemplateTests.cs
+++ tests/Sharpliner.Tests/AzureDevOps/TemplateTests.cs
@@ -123,6 +123,7 @@ public class TemplateTests
               inputs:
                 command: build
                 projects: ${{ parameters.project }}
+
             - ${{ parameters.afterBuild }}
             """);
     }
