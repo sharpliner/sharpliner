ae792d89314f40e6f159992744397599b91b99ef
Tom Longhurst <thomhurst@gmail.com>
2023-01-31 19:03:54 +0000

    Support for LockBehavior

---
+++ src/Sharpliner/AzureDevOps/LockBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Sharpliner.AzureDevOps;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public enum LockBehaviour
+{
+    runLatest,
+    sequential
+}
---
+++ tests/Sharpliner.Tests/AzureDevOps/PipelineLockBehaviorTests.cs
@@ -0,0 +1,69 @@
+﻿using FluentAssertions;
+using Sharpliner.AzureDevOps;
+using Xunit;
+
+namespace Sharpliner.Tests.AzureDevOps;
+
+public class PipelineLockBehaviorTests
+{
+    private class PipelineLockBehaviorTests_Pipeline : SimpleTestPipeline
+    {
+        public PipelineLockBehaviorTests_Pipeline(LockBehaviour? lockBehavior)
+        {
+            Pipeline =  new()
+            {
+                Name = "LockBehaviorTest",
+                LockBehavior = lockBehavior
+            };
+        }
+        public override SingleStagePipeline Pipeline { get; }
+    }
+
+    [Fact]
+    public void PipelineLockBehavior_Serialization_Test_Sequential()
+    {
+        var yaml = new PipelineLockBehaviorTests_Pipeline(LockBehaviour.sequential).Serialize();
+
+        yaml.Trim().Should().Be(
+            """
+            name: LockBehaviorTest
+
+            lockBehavior: sequential
+            """);
+    }
+
+    [Fact]
+    public void PipelineLockBehavior_Serialization_Test_RunLatest()
+    {
+        var yaml = new PipelineLockBehaviorTests_Pipeline(LockBehaviour.runLatest).Serialize();
+
+        yaml.Trim().Should().Be(
+            """
+            name: LockBehaviorTest
+
+            lockBehavior: runLatest
+            """);
+    }
+
+    [Fact]
+    public void PipelineLockBehavior_Serialization_Test_Null()
+    {
+        var yaml = new PipelineLockBehaviorTests_Pipeline(null).Serialize();
+
+        yaml.Trim().Should().Be(
+            """
+            name: LockBehaviorTest
+            """);
+    }
+
+    [Fact]
+    public void PipelineLockBehavior_Serialization_Test_Default()
+    {
+        var yaml = new PipelineLockBehaviorTests_Pipeline(default).Serialize();
+
+        yaml.Trim().Should().Be(
+            """
+            name: LockBehaviorTest
+            """);
+    }
+}
