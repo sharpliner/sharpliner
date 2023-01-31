a0f01487dd0243210e4ddb1723be5e6a1d545b44
Tom Longhurst <thomhurst@gmail.com>
2023-01-31 16:01:33 +0000

    Add support for variable template references (#262)

--- src/Sharpliner/AzureDevOps/AzureDevOpsDefinition.cs
+++ src/Sharpliner/AzureDevOps/AzureDevOpsDefinition.cs
@@ -242,6 +242,12 @@ public abstract class AzureDevOpsDefinition
     /// <param name="name">Group name</param>
     protected static Conditioned<VariableBase> Group(string name) => new(new VariableGroup(name));
 
+    /// <summary>
+    /// References a variable template.
+    /// </summary>
+    /// <param name="name">Template name</param>
+    protected static Conditioned<VariableBase> Template(string name) => new(new VariableTemplate(name));
+
     #endregion
 
     #region Pipeline task shorthands
