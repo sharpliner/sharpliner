6f8598b35d24fbed519c4f0c05ab9e641b9160f6
Premek Vysoky <premek.vysoky@microsoft.com>
2023-01-31 23:02:37 +0100

    Fix API

--- tests/Sharpliner.Tests/PublicApiChangeTest.cs
+++ tests/Sharpliner.Tests/PublicApiChangeTest.cs
@@ -12,20 +12,20 @@ public class PublicApiChangeTest
     [Fact]
     public void PublicApisHaventChangedTest()
     {
-        var api = typeof(ISharplinerDefinition).Assembly.GeneratePublicApi().Trim();
-        var exportedApi = GetResourceFile(typeof(PublicApiChangeTest).Assembly, "PublicApiExport.txt").Trim();
+        var actualApi = typeof(ISharplinerDefinition).Assembly.GeneratePublicApi().Trim();
+        var expectedApi = GetResourceFile(typeof(PublicApiChangeTest).Assembly, "PublicApiExport.txt").Trim();
 
         // There can be some unimportant differences in the header
-        api = api.Substring(api.IndexOf("namespace ")).Trim();
-        exportedApi = exportedApi.Substring(exportedApi.IndexOf("namespace ")).Trim();
+        actualApi = actualApi.Substring(actualApi.IndexOf("namespace ")).Trim();
+        expectedApi = expectedApi.Substring(expectedApi.IndexOf("namespace ")).Trim();
 
-        if (api != exportedApi)
+        if (actualApi != expectedApi)
         {
             throw new Exception("Detected a change in the public API of the library. If the change is intentional, please update the PublicApiExport.txt file."
                 + Environment.NewLine
                 + "Expected content:"
                 + Environment.NewLine
-                + api);
+                + actualApi);
         }
     }
 
