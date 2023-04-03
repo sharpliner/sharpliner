91c0f7fa7c8c4d3e829c6df93be596e5b3fd9931
Premek Vysoky <premek.vysoky@microsoft.com>
2023-04-03 22:31:43 +0200

    Add a test for repository resources

---
+++ tests/Sharpliner.Tests/AzureDevOps/Model/PipelineResourceSerializationTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Sharpliner.AzureDevOps;
+using Xunit;
+
+namespace Sharpliner.Tests.AzureDevOps;
+
+public class PipelineResourceSerializationTests
+{
+    private class ResourcePipeline : SingleStagePipelineDefinition
+    {
+        public override string TargetFile => "foo.yaml";
+
+        public override SingleStagePipeline Pipeline => new()
+        {
+            Resources = new Resources()
+            {
+                Repositories =
+                {
+                    new RepositoryResource("sharpliner")
+                    {
+                        Type = RepositoryType.Git,
+                        Endpoint = "https://github.com/sharpliner.sharpliner",
+                    }
+                }
+            }
+        };
+    }
+
+    [Fact]
+    public void RepositoryResource_Serialization_Test()
+    {
+        string yaml = new ResourcePipeline().Serialize();
+        yaml.Trim().Should().Be(
+            """
+            resources:
+              repositories:
+              - repository: sharpliner
+                endpoint: https://github.com/sharpliner.sharpliner
+            """);
+    }
+}
