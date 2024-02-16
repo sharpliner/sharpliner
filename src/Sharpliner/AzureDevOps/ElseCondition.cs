41b4ea6ce9e5800f1356525cf4dd2f90c0e15ab4
Premek Vysoky <premek.vysoky@microsoft.com>
2024-02-16 22:15:21 +0100

    Add support for a stand-alone else statement

---
+++ src/Sharpliner/AzureDevOps/ElseCondition.cs
@@ -0,0 +1,8 @@
+﻿using Sharpliner.AzureDevOps.ConditionedExpressions;
+
+namespace Sharpliner.AzureDevOps;
+
+public class ElseCondition : Condition
+{
+    internal override string Serialize() => ElseTagStart + TagEnd;
+}
