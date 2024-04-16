22aa3cfd5e3f78b9002cd8b4926eecedf586d02d
Premek Vysoky <premek.vysoky@microsoft.com>
2024-04-16 21:59:33 +0200

    Add an easy to way to export the public API

---
+++ eng/PublicApiExporter/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO;
+using PublicApiGenerator;
+using Sharpliner;
+
+namespace PublicApiExporter;
+
+public class Program
+{
+    public static void Main()
+    {
+        var assembly = typeof(ISharplinerDefinition).Assembly;
+        var publicApi = assembly.GeneratePublicApi();
+        publicApi = publicApi.Substring(publicApi.IndexOf("namespace ")).Trim();
+        var exportApiPath = Path.Combine(assembly.Location, "..", "..", "..", "..", "..", "..", "tests", "Sharpliner.Tests", "PublicApiExport.txt");
+        File.WriteAllText(exportApiPath, publicApi);
+        Console.WriteLine("Public API exported to PublicApiExport.txt");
+    }
+}
--- tests/Sharpliner.Tests/PublicApiChangeTest.cs
+++ tests/Sharpliner.Tests/PublicApiChangeTest.cs
@@ -9,6 +9,11 @@ namespace Sharpliner.Tests;
 
 public class PublicApiChangeTest
 {
+    /// <summary>
+    /// This test ensures that the public API of the library hasn't changed.
+    /// If the API has changed, the PublicApiExport.txt file should be updated.
+    /// You can do so by running the PublicApiExporter project.
+    /// </summary>
     [Fact]
     public void PublicApisHaventChangedTest()
     {
