aed9f7d0b1258de5c33fdf50cd070593a7056bc2
Premek Vysoky <premek.vysoky@microsoft.com>
2024-04-18 20:52:29 +0200

    Fix CPM

--- tests/E2E.Tests/ProjectUsingTheLibrary/ProjectTestDefinition.cs
+++ tests/E2E.Tests/ProjectUsingTheLibrary/ProjectTestDefinition.cs
@@ -5,13 +5,12 @@ namespace Tests.ProjectUsingTheLibraryNuGet;
 
 // These NuGet.Tests are E2E testing following scenario:
 // 1. We create a "library" project with an arbitrary Sharpliner definition
-// 2. We publish a .nupkg of this library project
-// 3. We reference this .nupkg in another project
+// 2. We reference this library in another project directly
 
 // This is testing that all the referenced DLLs are loaded well even for this transitive scenario.
 // This is testing user scenarios where people want to publish definitions in the form of NuGet and re-use it in other projects.
 
-// This class is the one that uses the library NuGet
+// This class is the one that uses the library directly
 public class ProjectTestDefinition : BasePipelineFromLibrary
 {
     public override string TargetFile => Path.Combine("artifacts", "E2E.Tests", "ProjectTestDefinition.yml");
