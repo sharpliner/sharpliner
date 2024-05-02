f3ad097eb5aaf606d037a9d0b014bbe2d10d85fa
Přemek Vysoký <premek.vysoky@microsoft.com>
2024-05-02 15:35:31 +0200

    Fix the workspaces model (#310)
    * Fix the workspaces model
    Resolves https://github.com/sharpliner/sharpliner/issues/309
    
    * Export public API

---
+++ tests/Sharpliner.Tests/AzureDevOps/Model/WorkspaceTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Sharpliner.AzureDevOps;
+using Xunit;
+
+namespace Sharpliner.Tests.AzureDevOps;
+
+public class WorkspaceTests
+{
+    private class WorkspacePipeline : SimpleTestPipeline
+    {
+        public override SingleStagePipeline Pipeline => new()
+        {
+            Jobs =
+            {
+                new Job("job1")
+                {
+                    Workspace = JobWorkspace.Resources,
+                }
+            }
+        };
+    }
+
+    [Fact]
+    public void Serialize_Pipeline_Test()
+    {
+        WorkspacePipeline pipeline = new();
+        string yaml = pipeline.Serialize();
+        yaml.Trim().Should().Be(
+        """
+        jobs:
+        - job: job1
+          workspace:
+            clean: resources
+        """);
+    }
+}
