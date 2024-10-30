0d641fd50fc3282b69284f69cd46e37d3b15c7db
Meir Blachman <meirblachman@gmail.com>
2024-10-31 01:04:27 +0900

    docs: conditions (#336)
    How can these be used? (no usage in the code)
    - `IfCondition Equal(IfCondition condition)`
    - `IfCondition NotEqual(IfCondition condition)`
    - `IfConditionBuilder<T>.Equal(string)`
    
    Related #325

--- src/Sharpliner/AzureDevOps/ConditionedExpressions/ConditionedDictionary.cs
+++ src/Sharpliner/AzureDevOps/ConditionedExpressions/ConditionedDictionary.cs
@@ -2,8 +2,17 @@
 
 namespace Sharpliner.AzureDevOps.ConditionedExpressions;
 
+/// <summary>
+/// A dictionary that can contain <see cref="Conditioned"/> items.
+/// </summary>
 public class ConditionedDictionary : Dictionary<string, object>
 {
+    /// <summary>
+    /// Adds a new item to the dictionary.
+    /// If the item is a <see cref="Conditioned"/> item, it will be marked as a single item.
+    /// </summary>
+    /// <param name="key">The key.</param>
+    /// <param name="item">The value.</param>
     public new void Add(string key, object item)
     {
         if (item is Conditioned conditioned)
