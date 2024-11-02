f8e9e0776775581a9024faa4e25aa752820cbaba
Premek Vysoky <premek.vysoky@microsoft.com>
2024-11-02 14:32:09 +0100

    TaskBuilderBase

--- src/Sharpliner/Common/Model/Tasks/TaskBuilderBase.cs
+++ src/Sharpliner/Common/Model/Tasks/TaskBuilderBase.cs
@@ -9,6 +9,13 @@ namespace Sharpliner.Common.Model.Tasks;
 /// </summary>
 public abstract class TaskBuilderBase
 {
+    /// <summary>
+    /// Helper method to load a resource file from the assembly.
+    /// This is used for example when inlining scripts into YAML which are included as embedded resources.
+    /// </summary>
+    /// <param name="assembly">Assembly the embedded resources is part of</param>
+    /// <param name="resourceFileName">Name of the embedded resource file</param>
+    /// <returns>Contents of an embedded resource file</returns>
     protected static string GetResourceFile(Assembly assembly, string resourceFileName)
     {
         Stream? stream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.{resourceFileName}");
