37f389865190e944844444fd577a8bc611295703
Premek Vysoky <premek.vysoky@microsoft.com>
2024-11-27 21:26:43 +0100

    Add test for enum variables

--- tests/Sharpliner.Tests/AzureDevOps/PipelineVariableTests.cs
+++ tests/Sharpliner.Tests/AzureDevOps/PipelineVariableTests.cs
@@ -6,6 +6,12 @@ namespace Sharpliner.Tests.AzureDevOps;
 
 public class PipelineVariableTests
 {
+    private enum Configuration
+    {
+        Debug,
+        Release
+    }
+
     private class PipelineVariableTests_Pipeline : SimpleTestPipeline
     {
         public override SingleStagePipeline Pipeline => new()
@@ -14,7 +20,10 @@ public class PipelineVariableTests
             {
                 Variable("SomeVariable", "Some Value"),
                 Group("SomeGroup"),
-                VariableTemplate("SomeTemplate")
+                VariableTemplate("SomeTemplate"),
+                Variable("SomeEnum1", Configuration.Release),
+                If.IsBranch("main")
+                    .Variable("SomeEnum2", Configuration.Debug)
             },
         };
     }
@@ -33,6 +42,13 @@ public class PipelineVariableTests
             - group: SomeGroup
 
             - template: SomeTemplate
+
+            - name: SomeEnum1
+              value: Release
+
+            - ${{ if eq(variables['Build.SourceBranch'], 'refs/heads/main') }}:
+              - name: SomeEnum2
+                value: Debug
             """);
     }
 }
