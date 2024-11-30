9f425e3d6e7740eb85b87ab82b73cc08c109ce19
Meir Blachman <meirblachman@gmail.com>
2024-11-30 20:25:45 +0200

    Update src/Sharpliner/AzureDevOps/Model/Tasks/Marketplace/NuGetCommandTask.cs
    Co-authored-by: Přemek Vysoký <premek.vysoky@microsoft.com>

--- src/Sharpliner/AzureDevOps/Model/Tasks/Marketplace/NuGetCommandTask.cs
+++ src/Sharpliner/AzureDevOps/Model/Tasks/Marketplace/NuGetCommandTask.cs
@@ -4,6 +4,10 @@ namespace Sharpliner.AzureDevOps.Tasks
 {
     /// <summary>
     /// Represents the abstract NuGetCommand@2 task in Azure DevOps pipelines.
+    /// Use this task to restore, pack, or push NuGet packages, or run a NuGet command.
+    /// This task supports NuGet.org and authenticated feeds like Azure Artifacts and MyGet.
+    /// This task also uses NuGet.exe and works with .NET Framework apps.
+    /// For .NET Core and .NET Standard apps, use the .NET Core task.
     /// </summary>
     public abstract record NuGetCommandTask : AzureDevOpsTask
     {
