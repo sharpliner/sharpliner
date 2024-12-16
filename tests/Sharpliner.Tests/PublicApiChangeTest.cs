e8d003e1655bd9ec4859321893978c71d60bbdd8
Meir Blachman <meirblachman@gmail.com>
2024-12-16 19:50:38 +0200

    remove PublicApiExporter

--- tests/Sharpliner.Tests/PublicApiChangeTest.cs
+++ tests/Sharpliner.Tests/PublicApiChangeTest.cs
@@ -7,7 +7,6 @@ public class PublicApiChangeTest
     /// <summary>
     /// This test ensures that the public API of the library hasn't changed.
     /// If the API has changed, the PublicApiExport.txt file should be updated.
-    /// You can do so by running the PublicApiExporter project.
     /// </summary>
     [Fact]
     public Task PublicApisHaventChangedTest()
