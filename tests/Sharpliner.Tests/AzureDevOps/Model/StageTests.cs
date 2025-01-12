c82e1b178b1b930f6bc3814677fb66695f12a99e
David Gardiner <david@gardiner.net.au>
2025-01-13 03:17:26 +1030

    Allow Stage.DisplayName to be conditional (#425)

---
+++ tests/Sharpliner.Tests/AzureDevOps/Model/StageTests.cs
@@ -0,0 +1,28 @@
+﻿using Sharpliner.AzureDevOps;
+
+namespace Sharpliner.Tests.AzureDevOps;
+
+public class StageTests
+{
+    private class StagePipeline : PipelineDefinition
+    {
+        public override string TargetFile => "stage-pipeline";
+        public override Pipeline Pipeline => new()
+        {
+            Stages =
+            [
+                Stage("Stage1")
+                    .DisplayAs("DisplayName")
+                    
+            ]
+        };
+    }
+
+    [Fact]
+    public Task ResourcePipeline_Serialization_Test()
+    {
+        var pipeline = new StagePipeline();
+
+        return Verify(pipeline.Serialize());
+    }
+}
