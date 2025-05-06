bf023982e1144b2577c4fdf03de59e62768c89ad
Michael McGaha <mmcgaha@microsoft.com>
2025-05-06 15:25:49 -0700

    add tests

---
+++ tests/Sharpliner.Tests/AzureDevOps/Model/Tasks/Marketplace/AzureCliTaskTests.cs
@@ -0,0 +1,30 @@
+﻿using Sharpliner.AzureDevOps.Tasks;
+
+namespace Sharpliner.Tests.AzureDevOps;
+
+public class AzureCliTaskTests
+{
+    [Fact]
+    public Task Serialize_Task_Test()
+    {
+        var task = new AzureCliTask("connectedServiceNameARM", ScriptType.Ps, ScriptLocation.InlineScript)
+        {
+            InlineScript = "Write-Host \"test\"",
+            Arguments = "arg1 arg2",
+            PowerShellErrorActionPreference = PowerShellErrorActionPreference.SilentlyContinue
+        };
+
+        return Verify(SharplinerSerializer.Serialize(task));
+    }
+
+    [Fact]
+    public Task Serialize_Task_With_Defaults_Test()
+    {
+        var task = new AzureCliTask("connectedServiceNameARM", ScriptType.Ps, ScriptLocation.InlineScript)
+        {
+            InlineScript = "Write-Host \"test\"",
+        };
+
+        return Verify(SharplinerSerializer.Serialize(task));
+    }
+}
