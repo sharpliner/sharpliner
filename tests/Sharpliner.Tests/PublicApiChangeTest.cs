f6993d8d62fc866052f0c234a73dc175e88dc0e0
Premek Vysoky <premek.vysoky@microsoft.com>
2026-05-27 16:35:33 +0200

    Split also test projects

--- tests/Sharpliner.Tests/PublicApiChangeTest.cs
+++ tests/Sharpliner.Tests/PublicApiChangeTest.cs
@@ -1,23 +1,9 @@
-﻿using PublicApiGenerator;
+using PublicApiGenerator;
 
 namespace Sharpliner.Tests;
 
 public class PublicApiChangeTest
 {
-    /// <summary>
-    /// This test ensures that the public API of the Sharpliner.Core library hasn't changed.
-    /// If the API has changed, the PublicApiExport.Sharpliner.Core.txt file should be updated.
-    /// </summary>
-    [Fact]
-    public Task SharplinerCorePublicApisHaventChangedTest()
-    {
-        var publicApi = typeof(ISharplinerDefinition).Assembly.GeneratePublicApi();
-
-        return Verify(publicApi)
-            .UseFileName("PublicApiExport.Sharpliner.Core.txt")
-            .UseDirectory(".");
-    }
-
     /// <summary>
     /// This test ensures that the public API of the Sharpliner library (the MSBuild task)
     /// hasn't changed. If the API has changed, the PublicApiExport.Sharpliner.txt file should
